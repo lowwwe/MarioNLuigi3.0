536e904f7beb25f544e22694d9c61f4d8a653b73
Peter Lowe <peter.lowe@itcarlow.ie>
2025-05-14 11:21:09 +0100

    first commit

---
+++ Mario N Luigi/Mario N Luigi/Game.cpp
@@ -0,0 +1,180 @@
+/// <summary>
+/// author Pete Lowe May 2025
+/// you need to change the above line or lose marks
+/// Mario N Luigi game
+/// </summary>
+
+
+#include "Game.h"
+#include <iostream>
+
+
+
+/// <summary>
+/// default constructor
+/// setup the window properties
+/// load and setup the texts
+/// load and setup the images
+/// load and setup the sounds
+/// </summary>
+Game::Game() :
+	m_window{ sf::VideoMode{ sf::Vector2u{800U, 600U}, 32U }, "Mario N Luigi" },
+	m_DELETEexitGame{false} //when true game will exit
+{
+	setupTexts(); // load font 
+	setupSprites(); // load texture
+	setupAudio(); // load sounds
+}
+
+/// <summary>
+/// default destructor we didn't dynamically allocate anything
+/// so we don't need to free it, but mthod needs to be here
+/// </summary>
+Game::~Game()
+{
+}
+
+
+/// <summary>
+/// main game loop
+/// update 60 times per second,
+/// process update as often as possible and at least 60 times per second
+/// draw as often as possible but only updates are on time
+/// if updates run slow then don't render frames
+/// </summary>
+void Game::run()
+{	
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	const float fps{ 60.0f };
+	sf::Time timePerFrame = sf::seconds(1.0f / fps); // 60 fps
+	while (m_window.isOpen())
+	{
+		processEvents(); // as many as possible
+		timeSinceLastUpdate += clock.restart();
+		while (timeSinceLastUpdate > timePerFrame)
+		{
+			timeSinceLastUpdate -= timePerFrame;
+			processEvents(); // at least 60 fps
+			update(timePerFrame); //60 fps
+		}
+		render(); // as many as possible
+	}
+}
+/// <summary>
+/// handle user and system events/ input
+/// get key presses/ mouse moves etc. from OS
+/// and user :: Don't do game update here
+/// </summary>
+void Game::processEvents()
+{
+	
+	while (const std::optional newEvent = m_window.pollEvent())
+	{
+		if ( newEvent->is<sf::Event::Closed>()) // close window message 
+		{
+			m_DELETEexitGame = true;
+		}
+		if (newEvent->is<sf::Event::KeyPressed>()) //user pressed a key
+		{
+			processKeys(newEvent);
+		}
+	}
+}
+
+
+/// <summary>
+/// deal with key presses from the user
+/// </summary>
+/// <param name="t_event">key press event</param>
+void Game::processKeys(const std::optional<sf::Event> t_event)
+{
+	const sf::Event::KeyPressed *newKeypress = t_event->getIf<sf::Event::KeyPressed>();
+	if (sf::Keyboard::Key::Escape == newKeypress->code)
+	{
+		m_DELETEexitGame = true; 
+	}
+}
+
+/// <summary>
+/// Check if any keys are currently pressed
+/// </summary>
+void Game::checkKeyboardState()
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape))
+	{
+		m_DELETEexitGame = true; 
+	}
+}
+
+/// <summary>
+/// Update the game world
+/// </summary>
+/// <param name="t_deltaTime">time interval per frame</param>
+void Game::update(sf::Time t_deltaTime)
+{
+	checkKeyboardState();
+	if (m_DELETEexitGame)
+	{
+		m_window.close();
+	}
+}
+
+/// <summary>
+/// draw the frame and then switch buffers
+/// </summary>
+void Game::render()
+{
+	m_window.clear(ULTRAMARINE);
+
+	m_window.draw(m_DELETElogoSprite);
+	m_window.draw(m_DELETEwelcomeMessage);
+	
+	m_window.display();
+}
+
+/// <summary>
+/// load the font and setup the text message for screen
+/// </summary>
+void Game::setupTexts()
+{
+	if (!m_jerseyFont.openFromFile("ASSETS\\FONTS\\Jersey20-Regular.ttf"))
+	{
+		std::cout << "problem loading arial black font" << std::endl;
+	}
+	m_DELETEwelcomeMessage.setFont(m_jerseyFont);
+	m_DELETEwelcomeMessage.setString("SFML Game");
+	m_DELETEwelcomeMessage.setPosition(sf::Vector2f{ 205.0f, 240.0f });
+	m_DELETEwelcomeMessage.setCharacterSize(96U);
+	m_DELETEwelcomeMessage.setOutlineColor(sf::Color::Black);
+	m_DELETEwelcomeMessage.setFillColor(sf::Color::Red);
+	m_DELETEwelcomeMessage.setOutlineThickness(2.0f);
+
+}
+
+/// <summary>
+/// load the texture and setup the sprite for the logo
+/// </summary>
+void Game::setupSprites()
+{
+	if (!m_DELETElogoTexture.loadFromFile("ASSETS\\IMAGES\\SFML-LOGO.png"))
+	{
+		// simple error message if previous call fails
+		std::cout << "problem loading logo" << std::endl;
+	}
+	
+	m_DELETElogoSprite.setTexture(m_DELETElogoTexture,true);// to reset the dimensions of texture
+	m_DELETElogoSprite.setPosition(sf::Vector2f{ 150.0f, 50.0f });
+}
+
+/// <summary>
+/// load sound file and assign buffers
+/// </summary>
+void Game::setupAudio()
+{
+	if (!m_DELETEsoundBuffer.loadFromFile("ASSETS\\AUDIO\\beep.wav"))
+	{
+		std::cout << "Error loading beep sound" << std::endl;
+	}
+	m_DELETEsound.play(); // test sound
+}
---
+++ Mario N Luigi/Mario N Luigi/Game.h
@@ -0,0 +1,60 @@
+/// <summary>
+/// author Pete Lowe May 2025
+/// you need to change the above line or lose marks
+/// Also don't have any member properties called Delete...
+/// </summary>
+#ifndef GAME_HPP
+#define GAME_HPP
+#pragma warning( push )
+#pragma warning( disable : 4275 )
+// ignore this warning
+// C:\SFML - 3.0.0\include\SFML\System\Exception.hpp(41, 47) : 
+// warning C4275 : non dll - interface class 'std::runtime_error' used as base for dll - interface class 'sf::Exception'
+
+/// <summary>
+/// include guards used so we don't process this file twice
+/// same as #pragma once
+/// Don't forget the #endif at the bottom
+/// </summary>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+
+const sf::Color ULTRAMARINE{ 5, 55,242,255 }; // const colour
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	void run();
+
+private:
+
+	void processEvents();
+	void processKeys(const std::optional<sf::Event> t_event);
+	void checkKeyboardState();
+	void update(sf::Time t_deltaTime);
+	void render();
+	
+	void setupTexts();
+	void setupSprites();
+	void setupAudio();
+
+	
+	sf::RenderWindow m_window; // main SFML window
+	sf::Font m_jerseyFont;// font used by message
+	
+	sf::Text m_DELETEwelcomeMessage{ m_jerseyFont }; // text used for message on screen
+	sf::Texture m_DELETElogoTexture;//  texture used for sfml logo
+	sf::Sprite m_DELETElogoSprite{ m_DELETElogoTexture }; // sprite used for sfml logo
+	
+	sf::SoundBuffer m_DELETEsoundBuffer; // buffer for beep sound
+	sf::Sound m_DELETEsound{ m_DELETEsoundBuffer }; // sound object to play
+	bool m_DELETEexitGame; // control exiting game
+
+};
+
+#pragma warning( pop ) 
+#endif // !GAME_HPP
+
